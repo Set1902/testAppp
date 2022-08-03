9599c8825a2031cd91ec922ed6f9fa0589a9337f
Sergei Kovalev <skovalev1902@gmail.com>
2022-08-03 14:10:35 +0300

    Initial Commit

---
+++ TestAppp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppp
+//
+//  Created by Sergei Kovalev on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
